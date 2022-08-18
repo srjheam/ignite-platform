64558552427d1d2133a4a19b540217b33075b8e1
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-08-17 23:09:59 -0300

    Query for LessonsCard

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,31 @@
+import { gql, useQuery } from "@apollo/client";
 import { LessonCard } from "./LessonCard";
 
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      availableAt
+      title
+      slug
+    }
+  }
+`
+
+interface GetLessonsQueryResponse {
+  lessons: {
+    id: string
+    title: string
+    slug: string
+    availableAt: string
+    lessonType: 'live' | 'class'
+  }[]
+}
+
 export function Sidebar() {
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
@@ -8,12 +33,17 @@ export function Sidebar() {
       </span>
 
       <div className="flex flex-col gap-8">
-        <LessonCard
-          title='Abertura'
-          availableAt={new Date()}
-          slug='abertura'
-          type='class'
-        />
+        {data?.lessons.map(lesson => {
+          return (
+            <LessonCard
+              key={lesson.id}
+              availableAt={new Date(lesson.availableAt)}
+              slug={lesson.slug}
+              title={lesson.title}
+              type={lesson.lessonType}
+            />
+          )
+        })}
       </div>
     </aside>
   )
