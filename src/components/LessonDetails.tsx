5b5a806e0ddd623b02843a4330b699962463634c
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-08-29 11:05:28 -0300

    Add Teacher info to LessonDetails

--- src/components/LessonDetails.tsx
+++ src/components/LessonDetails.tsx
@@ -13,9 +13,22 @@ export function LessonDetails() {
             <h1 className="text-2xl font-bold">
               Aberturaaaaa
             </h1>
-            <p className="mt-4 text-gray-200">
+            <p className="mt-4 text-gray-200 leading-relaxed">
               Praahasdadjasndaskdnasdasndaskdnasdlaskdnlaksdnaslldn
             </p>
+          
+            <div className="flex items-center gap-4 mt-6">
+              <img
+                src="https://github.com/srjheam.png"
+                alt="Teacher's profile picture"
+                className="h-16 w-16 rounded-full border-2 border-blue-500"
+              />
+
+              <div className="leading-relaxed">
+                <strong className="font-bold text-2xl block">Jheam BláBlá</strong>
+                <span className="text-gray-200 text-sm block">He's the one</span>
+              </div>
+            </div>
           </div>
 
           <div className="flex flex-col gap-4">
