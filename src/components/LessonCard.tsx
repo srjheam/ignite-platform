f55c80f9c5201af24d4156bcda038c1cc60bd208
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-08-31 11:10:44 -0300

    Change LessonCard style

--- src/components/LessonCard.tsx
+++ src/components/LessonCard.tsx
@@ -1,7 +1,7 @@
 import { format, isPast } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import { CheckCircle, Lock } from 'phosphor-react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 interface LessonProps {
   title: string
@@ -11,11 +11,14 @@ interface LessonProps {
 }
 
 export function LessonCard(props: LessonProps) {
+  const { slug } = useParams<{ slug: string }>()
+
   const isLessonAvaiable = isPast(props.availableAt);
   const availableDateFormated = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
     locale: ptBR
   })
-  
+  const isActiveLesson = slug === props.slug
+
   return (
     <div>
       <span className="text-gray-300">
@@ -24,15 +27,19 @@ export function LessonCard(props: LessonProps) {
 
       <Link
         to={`/event/lessons/${props.slug}`}
-        className={`rounded border border-gray-500 p-4 mt-2 block ${
-          isLessonAvaiable
-          ? "hover:border-green-500 transition-colors"
-          : "pointer-events-none"}
+        className={`relative rounded border border-gray-500 p-4 mt-2 block
+          ${isLessonAvaiable
+            ? "hover:border-green-500 transition-colors"
+            : "pointer-events-none"
+          }
+          ${isActiveLesson && "bg-green-500 border-green-500 before:content-[''] before:block before:absolute before:top-[50%] before:left-[-7px] before:w-[13.75px] before:h-[13.75px] before:rotate-45 before:translate-y-[-50%] before:bg-green-500 before:rounded-sm"}
         `}
       >
         <header className="flex items-center justify-between">
           {isLessonAvaiable ? (
-              <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+              <span className={`text-sm text-blue-500 font-medium flex items-center gap-2
+                ${isActiveLesson && "text-white"}
+              `}>
                 <CheckCircle size={20} />
                 Conteúdo Liberado
               </span>
@@ -43,12 +50,16 @@ export function LessonCard(props: LessonProps) {
               </span>
             )
           }
-          <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold     ">
+          <span className={`text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold
+            ${isActiveLesson && "border-white"}
+          `}>
             {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
           </span>
         </header>
 
-        <span className="font-bold text-gray-200 mt-5 block">
+        <span className={`font-bold text-gray-200 mt-5 block
+          ${isActiveLesson && "text-white"}
+        `}>
           {props.title}
         </span>
       </Link>
