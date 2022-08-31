5747498c0b1e92bc43337ebb742788f48d956bfa
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-08-31 11:10:27 -0300

    Add Subscribe action

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,6 +2,7 @@ interface InputProps {
   type: React.HTMLInputTypeAttribute
   placeholder?: string
   required?: boolean
+  onChange?: React.ChangeEventHandler<HTMLInputElement>
 }
 
 export function Input(props: InputProps) {
@@ -10,6 +11,7 @@ export function Input(props: InputProps) {
       type={props.type}
       placeholder={props.placeholder}
       required={props.required}
+      onChange={props.onChange}
       className="bg-gray-900 rounded px-5 h-14 outline-none border border-[transparent] focus:border-green-300 [&:not(:placeholder-shown):invalid]:border-red-500 transition-colors"
     />
   )
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,8 +1,40 @@
+import { gql, useMutation } from "@apollo/client";
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import { Logo } from "../components/Icon";
 import { Input } from "../components/Input";
 
+const CREATE_SUBSCRIBER_MUTATION = gql`
+  mutation CreateSubscriber ($name: String!, $email: String!) {
+    createSubscriber(data: {name: $name, email: $email}) {
+      id
+    }
+  }
+`
+
 export function Subscribe() {
+  const navigate = useNavigate()
+  
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  
+  const [createSubscriber, { loading }] = useMutation(CREATE_SUBSCRIBER_MUTATION)
+
+  async function handleSubscribe(e: FormEvent) {
+
+    e.preventDefault()
+
+    await createSubscriber({
+      variables: {
+        name,
+        email
+      }
+    })
+
+    navigate('/event')
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="flex-1 bg-blur bg-cover bg-no-repeat flex flex-col items-center">
@@ -18,12 +50,13 @@ export function Subscribe() {
           </div>
           <div className="w-full p-8 mr-0 bg-gray-700 border border-gray-500 rounded lg:w-auto lg:mr-4">
             <h2 className="text-2xl mb-6 block">Inscreva-se gratuitamente</h2>
-            <form action="" className="flex flex-col gap-2 w-full">
-              <Input type="text" placeholder="Seu nome completo" required={true} />
-              <Input type="email" placeholder="Digite seu e-mail" required={true} />
+            <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
+              <Input type="text" placeholder="Seu nome completo" required={true} onChange={e => setName(e.target.value)} />
+              <Input type="email" placeholder="Digite seu e-mail" required={true} onChange={e => setEmail(e.target.value)} />
               <button
                 type="submit"
-                className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+                disabled={loading}
+                className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:pointer-events-none disabled:bg-green-700 disabled:opacity-50"
               >
                 Garantir minha vaga
               </button>
